9537ce0e2df6b644850e4c679c897c138dde02ea
Serena Leung <serenaleung1@gmail.com>
2022-08-16 18:11:49 -0700

    Basic data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,17 @@
-import React from "react";
-import { Box, Typography } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { Box, Typography, FormControl, Select, MenuItem } from "@mui/material";
 
 function Home() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch(`https://api.lever.co/v0/postings/paralleldomain?mode=json`)
+      .then((res) => res.json())
+      .then((json) => {
+        setData(json);
+      })
+      .catch((error) => console.log("error", error))
+  }, []);
+
   return (
     <Box sx={{ maxWidth: "1024px", width: "100%", margin: "0 auto", py: 5 }}>
       <Typography variant="h3" sx={{ textAlign: "center", mb: 2 }}>
@@ -13,6 +23,34 @@ function Home() {
         Our data is training and testing autonomous systems at companies around
         the world. We're looking for talented visionaries to help us to expand
         our impact on the way artificial intelligence is developed.
+      </Typography>
+      <Typography>Filter By:</Typography>
+      <FormControl>
+        <Select
+          renderValue={(selected) => {
+            if (selected.length === 0) {
+              return <em>Placeholder</em>;
+            }
+            return selected.join(', ');
+          }}
+          inputProps={{ 'aria-label': 'Without label' }}
+        >
+          <MenuItem value="">
+            Placeholder
+          </MenuItem>
+          {data.map((d, index) => (
+            <MenuItem
+              key={index}
+              value={index}
+            >
+              {d.categories.location}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <Typography>{data.map(d => {
+        return d.categories.location
+      })}
       </Typography>
     </Box>
   );
